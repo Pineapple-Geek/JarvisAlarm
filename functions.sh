ba6654f70000a02b05b98b824e489a16a57853ba
Pineapple-Geek <naim.jebbour@outlook.fr>
2017-09-15 06:07:18 +0200

    Fonction reveil

--- functions.sh
+++ functions.sh
@@ -8,7 +8,35 @@
 # You can use translations provided in the language folders functions.sh
 
 pg_alarm_main () {
-	RH="$1"
-    RM="$2"
     say "$(pg_jarvis-alarm_lang "alarm_hours" "$1" "$2")" 
+   	RH="$1"
+    RM="$2"
+
+    T=1
+
+	while [ 1 -eq $T ];do
+
+	    DAY=`date`
+
+	    set -- $DAY
+	    TODAY=$1
+
+	    HDATE=`date`
+	    set -- $HDATE
+	    HHOUR=$5
+
+	    H=`echo $HHOUR | awk -F : '{print $1}'`
+	    M=`echo $HHOUR | awk -F : '{print $2}'`
+
+		    if [ "$H" == "$RH" ] && [ "$M" == "$RM" ]
+		    then
+			    echo REVEIL
+		    fi
+
+		    if [ $M -ne $RM ]
+		    then
+			    T=2
+		    fi
+
+	done
 }
