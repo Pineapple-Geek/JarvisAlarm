b20948e89362d8a241386538ed54176fed40abc7
Pineapple-Geek <naim.jebbour@outlook.fr>
2017-09-19 04:06:33 +0200

    Méthode Sleep

--- functions.sh
+++ functions.sh
@@ -8,17 +8,24 @@
 # You can use translations provided in the language folders functions.sh
 
 pg_alarm_main () {
-    say "$(pg_jarvis-alarm_lang "alarm_hours" "$1" "$2")" 
+say "$(pg_jarvis-alarm_lang "alarm_hours" "$1" "$2")" 
 
-    T=1
+# convert wakeup time to seconds
+target_h="$1"
+target_m="$2"
+target_s_t=`dc -e "$target_h 60 60 ** $target_m 60 *+p"`
 
-	while [ 1 -eq $T ];do
-		
-		    if [ date +%H = "$1" ] && [ date +%M = "$2" ]
-		    then
-			    say "Il est l'heure de se lever!"
-			    T=2
-		    fi
+# get current time and convert to seconds
+clock=`date | awk '{print $4}'`
+clock_h=`echo $clock | awk -F: '{print $1}'`
+clock_m=`echo $clock | awk -F: '{print $2}'`
+clock_s=`echo $clock | awk -F: '{print $3}'`
+clock_s_t=`dc -e "$clock_h 60 60 ** $clock_m 60 * $clock_s ++p"`
 
-	done
+# calculate difference in times, add number of sec. in day and mod by same
+sec_until=`dc -e "24 60 60 **d $target_s_t $clock_s_t -+r%p"`
+
+say "L'alarme disparaîtra à $target."
+
+sleep $sec_until
 }
