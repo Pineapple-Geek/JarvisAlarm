11021d0ad2bf3cd593f6a73279bb6c313a5afaf0
Pineapple-Geek <naim.jebbour@outlook.fr>
2017-10-10 14:28:43 +0200

    Correctif 2

--- functions.sh
+++ functions.sh
@@ -29,8 +29,8 @@ pg_reveil_main_fr () {
 
 date="$1:$2 Tomorrow"
 
-say "L'alarme est programmé pour le $(date --date="$date")."
-say "Veuillez attendre la fin de l'alarme pour donner d'autre instructions."
+say "Le réveil est programmé pour le $(date --date="$date")."
+say "Veuillez attendre la fin du réveil pour donner d'autre instructions."
 sleep $(( $(date --date="$date" +%s) - $(date +%s) ));
 
 if [ $wake_music != "null" ]
