14af3e82114a13d9d3c65df8919ab3501f247e90
Pineapple-Geek <naim.jebbour@outlook.fr>
2017-10-10 13:02:10 +0200

    Test

--- functions.sh
+++ functions.sh
@@ -11,9 +11,11 @@ pg_alarm_main_fr () {
 say "$(pg_jarvis-alarm_fr "alarm_hours" "$1" "$2")" 
 
 # convert wakeup time to seconds
-target_h=$(($1*3600))
-target_m=$(($2*60))
-target_s_t=$(($target_h + $target_m))
+target_h=$("$1")
+target_m=$("$2")
+let "target_H = target_h * 3600"
+let "target_M = target_m * 3600"
+target_s_t=$(($target_H + $target_M))
 
 # get current time and convert to seconds
 clock_h=$(date +%H)
