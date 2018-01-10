84593ba73cd86dbad51e974881b2cd2985c999c9
Pineapple-Geek <naim.jebbour@outlook.fr>
2018-01-10 15:17:52 +0100

    Correctif 1

--- functions.sh
+++ functions.sh
@@ -13,21 +13,21 @@ say "$(pg_jarvis-alarm_fr "alarm_hours" "$1" "$2")"
 say "Veuillez attendre la fin de l'alarme pour donner d'autre instructions."
 
 if [[ $1 < "12" ]]; then
-	target_h = "$1"
-	target_m = "$2"
-	target_t = "AM"
+	target_h="$1"
+	target_m="$2"
+	target_t="AM"
 fi
 
 if [[ $1 = "12" ]]; then
-	target_h = "1"
-	target_m = "$2"
-	target_t = "PM"
+	target_h="1"
+	target_m="$2"
+	target_t="PM"
 fi
 
 if [[ $1 > "12" ]]; then
 	target_h=$(($1 - 12))
-	target_m = "$2"
-	target_t = "PM"
+	target_m="$2"
+	target_t="PM"
 fi
 
 at target_h:target_m target_t
