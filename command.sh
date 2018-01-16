eed34ec7cafa337bceaf463d0f8040173da5e106
Pineapple-Geek <naim.jebbour@outlook.fr>
2018-01-16 14:19:15 +0100

    Update command.sh

--- command.sh
+++ command.sh
@@ -7,7 +7,7 @@ amixer -c 1 set Master ${volume}% #Demarrage du volume au minimal
 mpg321 plugins_installed/jarvis-alarm-test/audio/melodie.mp3 & #Lancement de la musique  
 while [ "$volume" -lt "$MAX_VOLUME" ] ; do 
    amixer -c 1 set Master ${volume}% 
-   ((volume=$volume+5)) 
+   volume=$((volume+5))
    killall qiv #Commande pour empêcher les fenêtres de s'empiler  
    sleep 1 # décider de combien de temps le processus va s'arreter entre les augmentations de volume
 done 
