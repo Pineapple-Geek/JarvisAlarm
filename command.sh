4227483f109c82bbeced052c6c22e61e90eda2e3
Pineapple-Geek <naim.jebbour@outlook.fr>
2018-01-16 13:19:33 +0100

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,1 +1,10 @@
-mpg321 plugins_installed/jarvis-alarm-test/audio/melodie.mp3
+volume=1 
+MAX_VOLUME=75 #don't blow your eardrums 
+amixer sset PCM ${volume}% #set the volume low first 
+mpg321 plugins_installed/jarvis-alarm-test/audio/melodie.mp3 & #you could do random stuff to select from a directory too  
+while [ "$volume" -lt "$MAX_VOLUME" ] ; do 
+   amixer sset PCM ${volume}% 
+   ((volume=$volume+1)) 
+   killall qiv #to keep the windows from stacking up  
+   sleep 10 # decide how long to sleep between volume increases 
+done 
